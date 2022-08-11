b76c939a2c8742e7267fd2ef7aad9821cff14b67
Tobi Balogun <tobi.balo@gmail.com>
2022-08-11 16:38:09 +0100

    Implement missing fields as database migration using Flask-Migrate

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -33,29 +33,36 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    name = db.Column(db.String(75), nullable=False)
+    genres = db.Column(db.ARRAY(db.String()), nullable=False)
+    city = db.Column(db.String(30), nullable=False)
+    state = db.Column(db.String(30), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    website = db.Column(db.String(90), nullable=False)
+    facebook_link = db.Column(db.String(90), nullable=True)
+    seeking_talent = db.Column(db.Boolean(), nullable=True)
+    seeking_description = db.Column(db.String(140), nullable=True)
+    image_link = db.Column(db.String(500), nullable=False)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate | 🚀
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    name = db.Column(db.String(75), nullable=False)
+    city = db.Column(db.String(30), nullable=False)
+    state = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    genres = db.Column(db.ARRAY(db.String()), nullable=False)
+    website = db.Column(db.String(90), nullable=False)
+    facebook_link = db.Column(db.String(90), nullable=True)
+    image_link = db.Column(db.String(500), nullable=False)
+    seeking_venue = db.Column(db.Boolean(), nullable=True)
+    seeking_description = db.Column(db.String(140), nullable=True)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate | 🚀
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
